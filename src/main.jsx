592e066f259e0422b20791e2cd30beda616f3b53
Ed_Chekanov <kide.da@gmail.com>
2025-12-09 21:27:01 +0300

    fix: deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/APP-To-Do-List">
+    <BrowserRouter basename="/APP-To-Do-List/">
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/APP-To-Do-List',
+  base: '/APP-To-Do-List/',
   plugins: [react()],
 });
