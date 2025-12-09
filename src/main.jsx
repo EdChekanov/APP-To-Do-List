7393a7771aecf89de744349b229856218da30f17
Ed_Chekanov <kide.da@gmail.com>
2025-12-09 21:23:05 +0300

    fix: deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/APP-To-Do-List">
       <App />
     </BrowserRouter>
   </StrictMode>
