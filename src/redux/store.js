201c18ce81fda00582e06e956298bebc7bf45b7c
Ed_Chekanov <kide.da@gmail.com>
2025-12-14 15:51:48 +0300

    fix: redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,13 @@ import { legacy_createStore, combineReducers } from 'redux';
 import inputTextReducer from './reducers/inputTextReducer';
 import tasksReducer from './reducers/tasksReducer';
 
-const store = legacy_createStore(
-  combineReducers(
-    {
-      tasks: tasksReducer,
-      inputText: inputTextReducer,
-    },
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  inputText: inputTextReducer,
+});
+
+const store = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? legacy_createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__())
+  : legacy_createStore(rootReducer);
 
 export default store;
